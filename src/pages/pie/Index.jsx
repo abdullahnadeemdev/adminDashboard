81d1ecc4bbaa2fc532faf565e71d7a2010e5595b
Abdullah Nadeem <abdullahnadeemdev1@gmail.com>
2026-01-22 12:47:21 +0500

    fixed naming of components

--- src/pages/pie/Index.jsx
+++ src/pages/pie/Index.jsx
@@ -1,6 +1,6 @@
 import { Box, Typography } from "@mui/material";
 import Header from "../../components/shared/header/Header";
-import MyPie from "../../components/scenes/pieChart/Index";
+import MyPie from "../../components/Charts/pieChart/Index";
 
 const Pie = () => {
   return (
